516ba29f9029433d809a0e31bb6ba5b1247d28f5
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2024-03-30 23:16:35 -0500

    Update write_config.py

--- write_config.py
+++ write_config.py
@@ -6,6 +6,9 @@ parser = argparse.ArgumentParser(description="Update model names in a YAML file.
 parser.add_argument("old_model", type=str, help="The old model name to be replaced.")
 parser.add_argument("new_model", type=str, help="The new model name to replace with.")
 parser.add_argument("write_dir", type=str, help="The new model name to replace with.")
+parser.add_argument("data_dir", type=str, help="The new model name to replace with.")
+
+
 
 args = parser.parse_args()
 # 读取YAML文件
@@ -22,6 +25,7 @@ try:
     data['model_name_or_path'] = args.old_model
     data['hub_model_id'] = args.old_model
     data['output_dir'] = args.new_model
+    data['dataset_splits'] = args.data_dir
 except:
     while True:
         print("ERROR!")
